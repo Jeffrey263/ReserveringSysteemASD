dcf1fec4f69b4efc59518851a8e45abdadcd013e
max <max.bosch@student.hu.nl>
2021-10-23 16:21:37 +0200

    Unit test ReserveringOphalen af

--- src/test/java/ReserveringOphalen.java
+++ src/test/java/ReserveringOphalen.java
@@ -1,8 +1,15 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import reserveringsysteem.reservering.adapters.ReserveringRESTService;
 import reserveringsysteem.reservering.application.ReserveringApplicationService;
 import reserveringsysteem.reservering.domain.DagdeelEnum;
+import reserveringsysteem.reservering.domain.Reservering;
 import reserveringsysteem.reservering.domain.ReserveringRepository;
+import reserveringsysteem.reservering.domain.ReserveringStatusEnum;
+
+import java.util.Date;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,17 +25,63 @@ public class ReserveringOphalen {
         app = new ReserveringApplicationService(rep);
         rest = new ReserveringRESTService(app);
 
-        //Voeg reserveringen toe die opgehaald kunnen worden
-        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2021"), DagdeelEnum.MIDDAG,1);
-        rest.createReservering(4, new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2021"), DagdeelEnum.MIDDAG,5);
-        rest.createReservering(2, new SimpleDateFormat("dd/MM/yyyy").parse("05/12/2021"), DagdeelEnum.MIDDAG,3);
-        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("05/12/2021"), DagdeelEnum.MIDDAG,5);
-        rest.createReservering(2, new SimpleDateFormat("dd/MM/yyyy").parse("10/12/2021"), DagdeelEnum.MIDDAG,7);
-        rest.createReservering(4, new SimpleDateFormat("dd/MM/yyyy").parse("10/12/2021"), DagdeelEnum.MIDDAG,5);
-        rest.createReservering(1, new SimpleDateFormat("dd/MM/yyyy").parse("20/12/2021"), DagdeelEnum.MIDDAG,2);
-        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("20/12/2021"), DagdeelEnum.MIDDAG,3);
-        rest.createReservering(2, new SimpleDateFormat("dd/MM/yyyy").parse("31/12/2021"), DagdeelEnum.MIDDAG,5);
-        rest.createReservering(2, new SimpleDateFormat("dd/MM/yyyy").parse("05/01/2022"), DagdeelEnum.MIDDAG,1);
-        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("06/01/2022"), DagdeelEnum.MIDDAG,5);
+        //geldige reservering
+        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2022"), DagdeelEnum.MIDDAG, 2);
     }
+
+
+    //lege lijst met reserveringen want verleden == geen geldige reserveringen
+    @Test
+    @Order(1)
+    public void testOphalen1() throws ParseException {
+        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2020"), DagdeelEnum.MIDDAG, 3);
+        Reservering r = (Reservering) rep.getById(3);
+        r.setStatus(ReserveringStatusEnum.VERLOPEN);
+        Assertions.assertEquals(0, rest.getAllGeldigeReserveringen(new SimpleDateFormat("dd/MM/yyyy").parse("01/11/2021"), new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2021")).size());
+    }
+
+    //geannuleerde reservering == lege lijst
+    @Test
+    @Order(2)
+    public void testOphalen2() throws ParseException {
+        rest.createReservering(5, new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2021"), DagdeelEnum.MIDDAG, 1);
+        Reservering r = (Reservering) rep.getById(1);
+        r.setStatus(ReserveringStatusEnum.GEANNULEERD);
+        Assertions.assertEquals(0, rest.getAllGeldigeReserveringen(new SimpleDateFormat("dd/MM/yyyy").parse("01/11/2021"), new SimpleDateFormat("dd/MM/yyyy").parse("02/12/2021")).size());
+
+    }
+
+
+    //testresultaat is een list met 1 id door 1 geldige reservering binnen de gespecificeerde datums
+    @Test
+    @Order(3)
+    public void testOphalen3() throws ParseException {
+        Assertions.assertEquals(1, rest.getAllGeldigeReserveringen(new SimpleDateFormat("dd/MM/yyyy").parse("01/11/2022"), new SimpleDateFormat("dd/MM/yyyy").parse("02/12/2022")).size());
+
+    }
+
+    //testresultaat is een list met 1 id van een geldige reservering op de gespecificeerde start datum
+    @Test
+    @Order(4)
+    public void testOphalen4() throws ParseException {
+        Assertions.assertEquals(1, rest.getAllGeldigeReserveringen(new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2022"), new SimpleDateFormat("dd/MM/yyyy").parse("02/12/2022")).size());
+    }
+
+
+    //testresultaat is een list met 1 id van een geldige reservering op de gespecificeerde eind datum
+    @Test
+    @Order(5)
+    public void testOphalen5() throws ParseException {
+        Assertions.assertEquals(1, rest.getAllGeldigeReserveringen(new SimpleDateFormat("dd/MM/yyyy").parse("01/11/2022"), new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2022")).size());
+    }
+
+
+    //testresultaat is 0 omdat eind datum voor begin datum ligt
+    @Test
+    @Order(6)
+    public void testOphalen6() throws ParseException {
+        Assertions.assertEquals(0, rest.getAllGeldigeReserveringen(new SimpleDateFormat("dd/MM/yyyy").parse("01/11/2022"), new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2021")).size());
+    }
+
+
 }
